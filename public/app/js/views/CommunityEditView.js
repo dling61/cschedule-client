d262944ae7e814bb521334ba9ace2c178a41ea78
kalyani <kalyani.ksnd@gmail.com>
2015-08-02 23:57:51 -0700

    Added fake users to be rendred in logged in user drop down

--- public/app/js/views/CommunityEditView.js
+++ public/app/js/views/CommunityEditView.js
@@ -1,40 +1,44 @@
-var CommunityEditView = Backbone.View.extend({
-    el: '.edit-community-div',
+	var CommunityEditView = Backbone.View.extend({
+		el: '.edit-community-div',
 
-    events: {
-    	'click .btn-edit-save': 'updateCommunity',
-    	"click .btn-edit-cancel": 'cancelEditCommunity'
-    },
+		communitynames:null,
 
-    initialize: function() {
-        var handler = _.bind(this.render, this);
+		events: {
+			'click .btn-edit-save': 'updateCommunity',
+			"click .btn-edit-cancel": 'cancelEditCommunity'
+		},
 
-        
-    },
-	
-    updateCommunity: function(){
-    	
-    	var name = $('.editCommunity input')[0].value
-        	//get innput name from the uder
-    	this.model.set('communityname', name);
-    	this.model.save();
-    	alert("you have successfully edited Community");
-    	$('.editCommunity').hide();
-    },
+		initialize: function() {
+			var handler = _.bind(this.render, this);
+		},
 
-    cancelEditCommunity:function() {
-         $('.editCommunity').hide();
-    },
+		updateCommunity: function(ev) {
+			
+			
+			var name = $('.editCommunity input')[0].value;
 
-    render: function(){
-    	//var name= this.model.get(communityname);
-    	//alert(name);
-    	$('.editCommunity input')[0].value = this.model.get("communityname")
-    	$('.editCommunity').show();
-		$('.createCommunity').hide();
-    	
-    	
-    	
-    }
+				 if( _.contains(this.communitynames,name)) {
+	              		alert("Enter valid community name,Community name already Exists");
+	               }  else {
+						this.model.set('communityname', name);
+						this.model.save();
+						alert(" successfully edited Community");
+		        }
+			
+			$('.editCommunity').hide();
+		},
 
-});
+		cancelEditCommunity: function() {
+			$('.editCommunity').hide();
+		},
+
+		render: function(communitynames) {
+			 var that = this;
+	        this.communitynames = communitynames
+			$('.editCommunity input')[0].value = this.model.get("communityname");
+			$('.editCommunity').show();
+			$('.createCommunity').hide();
+		}
+		
+
+	});
