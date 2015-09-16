cdb6d2609648280435eb92834928ef5de3b021bb
dling61 <dling61@yahoo.com>
2015-09-16 15:09:04 -0700

    add a directory for some common functions

---
+++ public/util/common.js
@@ -0,0 +1,16 @@
+
+
+    /**
+     * To get the latest Object Id
+     *
+     * @method
+     * @
+     */
+    function getNextObjectId(userId, eId) {
+       if (eId == 0) {
+	     return (userId * 10000 + 1);
+	   }
+	   else {
+          return eId + 1;
+	  
+    }
