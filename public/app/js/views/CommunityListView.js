039b06db7f349be2e543f5c906eacc3f70c4899b
teamguy <bill@billbiker.local>
2016-05-22 16:44:14 -0700

    reformat

--- public/app/js/views/CommunityListView.js
+++ public/app/js/views/CommunityListView.js
@@ -1,41 +1,41 @@
 define([
-	'underscore', 
-	'backbone',
-	'js/collections/communities',
-	'js/models/task',
+    'underscore',
+    'backbone',
+    'js/collections/communities',
+    'js/models/task',
     'js/views/CommunityView',
 
     'js/views/EventsView'
-], function(_, Backbone, Communities, Task, CommunityView, EventsView){
+], function(_, Backbone, Communities, Task, CommunityView, EventsView) {
 
-	// http://myapp.com/javascripts/jst.js
-	//var window;
+    // http://myapp.com/javascripts/jst.js
+    //var window;
 
-	window.JST = {};
+    window.JST = {};
 
-	window.JST['person/contact'] = _.template(
-		"<div class='contact'><%= name %> ..."
-	);
+    window.JST['person/contact'] = _.template(
+        "<div class='contact'><%= name %> ..."
+    );
 
-	window.JST['person/edit'] = _.template(
-		"<form method='post'><input type..."
-	);
-
-
-	window.JST['community/list'] = _.template(
-		  '<% _.each(communities, function(community) { %>'
-		+ '			<div class="community" data-id="<%=community.get("id")%>"> '
-		+ '				<div class="community-name" contenteditable="true">'
-		+ '                 <%=  htmlEncode(community.get("communityname")) %>'
-        + ' <div class="latestRnR">'
-        + '   <span style="color:blue; font-size:20pt;"> &#8226; </span>'
-        + '   Swap Request from Bill</div></div>'
-		+ '			</div>'
-		+ '		<% }); %>'
-	);
+    window.JST['person/edit'] = _.template(
+        "<form method='post'><input type..."
+    );
 
 
-	/*
+    window.JST['community/list'] = _.template(
+        '<% _.each(communities, function(community) { %>' 
+        + '			<div class="community" data-id="<%=community.get("id")%>"> ' 
+        + '				<div class="community-name" contenteditable="true">' 
+        + '                 <%=  htmlEncode(community.get("communityname")) %>' 
+        + '                 <div class="latestRnR">' 
+        + '                     <span style="color:blue; font-size:20pt;"> &#8226; </span>' 
+        + '                     Swap Request from Bill</div>' 
+        + '			</div></div>'     
+        + '		<% }); %>'
+    );
+
+
+    /*
 
 
 			<script type="text/template" id="community-dropdown-template">
@@ -48,189 +48,189 @@ define([
 
 
 
-	var CommunityListView = Backbone.View.extend({
+    var CommunityListView = Backbone.View.extend({
 
-		  el: '.community-list-view',
+        el: '.community-list-view',
 
-		  events: {
-			"dblclick .community": 'deleteCommunity',
-			"click .community": 'viewCommunity',
-			"click .community-name": 'onEditCommunity',
-			//"blur .community-name": 
-			"click .create-btn": 'showCreateCommunity',
-			"click .btn-create": 'createNewCommunity',
-			"click .btn-edit-cancel": "hideEditCommunity",
-			"click .cancel-create": "hideCreate",
-			 "click .page": "showPage",
-		  },
+        events: {
+            "dblclick .community": 'deleteCommunity',
+            "click .community": 'viewCommunity',
+            "click .community-name": 'onEditCommunity',
+            //"blur .community-name": 
+            "click .create-btn": 'showCreateCommunity',
+            "click .btn-create": 'createNewCommunity',
+            "click .btn-edit-cancel": "hideEditCommunity",
+            "click .cancel-create": "hideCreate",
+            "click .page": "showPage",
+        },
 
-		  initialize: function() {
-			// _.bindAll(this);
-			this.render();
-		  },
-		  showPage: function() {
-			$('.page').show();
+        initialize: function() {
+            // _.bindAll(this);
+            this.render();
+        },
+        showPage: function() {
+            $('.page').show();
 
-		  },
-
-		
-		  deleteCommunity: function(ev) {
-			var that = this;
-			alert("Are you sure you want to deleteCommunity");
-
-			var communityId = ev.target.getAttribute('data-id');  // ev.target.data('id')
-			communities = that.collection.where({
-			  "id": communityId
-			});
-			console.info(that.collection.toJSON());
-			communities[0].destroy({
-			  success: function() {
-				alert("you have  deleted community");
-
-			  },
-			  error: function(model, response) {
-
-				alert("Error in deleting Community " + response.statusText);
-			  }
-
-			});
-			var deletedcommunity = that.collection.remove(communities[0]);
-
-			console.info(this.collection.toJSON());
-			alert('you have deleted ' + name);
-			this.render();
-
-		  },
+        },
 
 
+        deleteCommunity: function(ev) {
+            var that = this;
+            alert("Are you sure you want to deleteCommunity");
 
-		  viewCommunity: function(ev) {
-			var communityView,
-			  communityId = ev.target.getAttribute('data-id'),
-			  communities = this.collection.where({
-				"id": communityId
-			  });
+            var communityId = ev.target.getAttribute('data-id'); // ev.target.data('id')
+            communities = that.collection.where({
+                "id": communityId
+            });
+            console.info(that.collection.toJSON());
+            communities[0].destroy({
+                success: function() {
+                    alert("you have  deleted community");
 
-			communityView = new CommunityView({
-			  model: communities[0]
-			});
+                },
+                error: function(model, response) {
 
-			//communityView.render();
+                    alert("Error in deleting Community " + response.statusText);
+                }
+
+            });
+            var deletedcommunity = that.collection.remove(communities[0]);
+
+            console.info(this.collection.toJSON());
+            alert('you have deleted ' + name);
+            this.render();
+
+        },
+
+
+
+        viewCommunity: function(ev) {
+            var communityView,
+                communityId = ev.target.getAttribute('data-id'),
+                communities = this.collection.where({
+                    "id": communityId
+                });
+
+            communityView = new CommunityView({
+                model: communities[0]
+            });
+
+            //communityView.render();
 
             //var eventView =  new EventsView();
 
             //eventView.render();
-			
 
 
-			//Test code for Events parsing
-              
-              if (false) {
-                    var eventsC = new EventsC();
-                    eventsC.fetch({
-                      success: function(events) {
-                          var taskm, assignmentm, tasksC, assignmentC;
-                          //parsing events to approximate collections(taks assignees)
 
-                          _.each(events.models, function(event){
-                              event.tasks = [];
-                              _.each(event.get("task"), function(taskAttributes){
+            //Test code for Events parsing
 
-                                  taskm = new Task(taskAttributes);
-                                  taskm.set("event-id",event.get("eventid"));
-                                  taskm.assignees = [];
-                                  _.each(taskAttributes.assignment, function(assignmentAttr){
-                                      // assignmentm = new AssignesM(assignmentAttr);
-                                      // assignmentm.set("task-id", taskm.get("taskid"));
-                                      // taskm.assignees.push(assignmentm);
-                                   }); //end of assignees
-                                   event.tasks.push(taskm);
+            if (false) {
+                var eventsC = new EventsC();
+                eventsC.fetch({
+                    success: function(events) {
+                        var taskm, assignmentm, tasksC, assignmentC;
+                        //parsing events to approximate collections(taks assignees)
 
-                              });//end of taks
-                          });//end of events
+                        _.each(events.models, function(event) {
+                            event.tasks = [];
+                            _.each(event.get("task"), function(taskAttributes) {
+
+                                taskm = new Task(taskAttributes);
+                                taskm.set("event-id", event.get("eventid"));
+                                taskm.assignees = [];
+                                _.each(taskAttributes.assignment, function(assignmentAttr) {
+                                    // assignmentm = new AssignesM(assignmentAttr);
+                                    // assignmentm.set("task-id", taskm.get("taskid"));
+                                    // taskm.assignees.push(assignmentm);
+                                }); //end of assignees
+                                event.tasks.push(taskm);
+
+                            }); //end of taks
+                        }); //end of events
 
 
                         //code to be used in view
 
-                      }
-                    });
-              }
+                    }
+                });
+            }
 
-			//Test code for Events parsing
-			
-		  },
+            //Test code for Events parsing
 
-		  createNewCommunity: function() {
-			  
-			  //WFB
-			var communitynamesArray = this.collection.pluck("communityname"),
-			 
-			  communityName = $('#input').val(),
-			  community;
+        },
 
-			if ( _.contains(communitynamesArray, communityName)) {
-			  alert("Enter valid community name,Community name already Exists");
-			} else {
-			  community = new Community({
-				ownerid: gLoginUser.ownerid, // same as login user id
-				communityname: $('#input').val(),
+        createNewCommunity: function() {
 
-				desp: "test communityname",
-				communityid: 90045
+            //WFB
+            var communitynamesArray = this.collection.pluck("communityname"),
 
-			  });
+                communityName = $('#input').val(),
+                community;
 
-			  community.save({}, {
-				success: function() {
-				  alert("you have successfully created community");
-				  $('.createCommunity').hide();
-				},
-				error: function(model, response) {
+            if (_.contains(communitynamesArray, communityName)) {
+                alert("Enter valid community name,Community name already Exists");
+            } else {
+                community = new Community({
+                    ownerid: gLoginUser.ownerid, // same as login user id
+                    communityname: $('#input').val(),
 
-				  alert("Error in creatiing Community " + response.statusText);
-				}
-			  });
-			}
-			$('.createCommunity').hide();
-		  },
-		  hideCreate: function() {
-			$('.createCommunity').hide();
-			$('.create-btn').show();
+                    desp: "test communityname",
+                    communityid: 90045
 
-		  },
+                });
 
-		  showCreateCommunity: function(ev) {
+                community.save({}, {
+                    success: function() {
+                        alert("you have successfully created community");
+                        $('.createCommunity').hide();
+                    },
+                    error: function(model, response) {
 
-			$('.createCommunity').show();
-			$('.create-btn').hide();
-		  },
+                        alert("Error in creatiing Community " + response.statusText);
+                    }
+                });
+            }
+            $('.createCommunity').hide();
+        },
+        hideCreate: function() {
+            $('.createCommunity').hide();
+            $('.create-btn').show();
 
-		  onEditCommunity: function(ev) {
+        },
 
-			ev.stopPropagation();
-			// var communitynamesArray = this.collection.pluck("communityname");
-			// var communityId = ev.target.parentElement.getAttribute('data-id');
-			// communities = this.collection.where({
-			//   "id": communityId
-			// });
+        showCreateCommunity: function(ev) {
 
-			// var communityEditView = new CommunityEditView({
-			//   model: communities[0]
-			// });
+            $('.createCommunity').show();
+            $('.create-btn').hide();
+        },
 
-			// communityEditView.render(communitynamesArray);
-			
-		  },
-		  
-		  hideEditCommunity: function() {
-			$('.editCommunity').hide();
+        onEditCommunity: function(ev) {
 
-		  },
+            ev.stopPropagation();
+            // var communitynamesArray = this.collection.pluck("communityname");
+            // var communityId = ev.target.parentElement.getAttribute('data-id');
+            // communities = this.collection.where({
+            //   "id": communityId
+            // });
 
-		  render: function() {
+            // var communityEditView = new CommunityEditView({
+            //   model: communities[0]
+            // });
 
-			var that = this;
-			/*
+            // communityEditView.render(communitynamesArray);
+
+        },
+
+        hideEditCommunity: function() {
+            $('.editCommunity').hide();
+
+        },
+
+        render: function() {
+
+            var that = this;
+            /*
 			this.chats = new CommunityChats();
 			this.chats.fetch({
 
@@ -256,41 +256,41 @@ define([
 			  }
 			});
 			*/
-			  
-			  
-			this.collection = new Communities();
 
-			this.collection.fetch({
 
-			  data: $.param({
-				ownerid: gLoginUser.ownerid   //user id 
-			  }),
-			  success: function(communityList) {
+            this.collection = new Communities();
 
-				//var template1 = _.template($('#community-list-template').html());
-				//var template2 = _.template($("#community-dropdown-template").html());
+            this.collection.fetch({
 
-				  
-	//var html = JST['person/edit']();
+                data: $.param({
+                    ownerid: gLoginUser.ownerid //user id 
+                }),
+                success: function(communityList) {
 
-	//var dict = { name: "Jason", email: "j.smith@gmail.com" };
-				  var template1 = JST['community/list']; //(dict);
-				  
-				  
-				$(".nav-body").html(template1({
-				  communities: that.collection.models
-				}));
-				  /* WFB
+                    //var template1 = _.template($('#community-list-template').html());
+                    //var template2 = _.template($("#community-dropdown-template").html());
+
+
+                    //var html = JST['person/edit']();
+
+                    //var dict = { name: "Jason", email: "j.smith@gmail.com" };
+                    var template1 = JST['community/list']; //(dict);
+
+
+                    $(".nav-body").html(template1({
+                        communities: that.collection.models
+                    }));
+                    /* WFB
 				$("#community-drop-down").html(template2({
 				  communities: that.collection.models
 				}));
 				*/
-			  }
-			})
-			
-		  },
+                }
+            })
+
+        },
 
 
-		});
-	return CommunityListView;
+    });
+    return CommunityListView;
 });
